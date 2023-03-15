03ea0fcdf282895acae70b0f4bf3bf4798e7e79b
NicolaeBunea <nicolae_bunea23@yahoo.com>
2023-03-15 23:27:21 +0200

    branch

---
+++ src/test/java/Loops/WhileLoop.java
@@ -0,0 +1,16 @@
+package Loops;
+
+import org.junit.Test;
+
+public class WhileLoop {
+    @Test
+    public void Execute(){
+    WhileExemple();
+    }
+    public Integer i=1;
+    public void WhileExemple(){
+        while (1<=10){
+            System.out.println(i);
+        }
+    }
+}
